0361c2fd9feb6898858b7626a10a49d27c12eaec
dmoke <dmytromykhaliukk@gmail.com>
2023-03-08 15:13:26 +0200

    login properties setup

--- src/test/java/utils/CSVUtils.java
+++ src/test/java/utils/CSVUtils.java
@@ -2,15 +2,19 @@ package utils;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
+import lombok.SneakyThrows;
 import models.LoginData;
 
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class CSVUtils {
-    private static final String CSV_FILE_PATH = "src/test/resources/positive_login_data.csv";
+    private static final String CSV_FILE_PATH = readProperties("src/test/java/properties/login.properties", "data");
 
     public static List<LoginData> readCSVFile() {
         List<LoginData> loginDataList = new ArrayList<>();
@@ -26,5 +30,13 @@ public class CSVUtils {
             e.printStackTrace();
         }
         return loginDataList;
+    }
+    @SneakyThrows
+    public static String readProperties(String path, String property){
+        Properties prop = new Properties();
+        InputStream input = new FileInputStream(path);
+        prop.load(input);
+
+        return prop.getProperty(property);
     }
 }
